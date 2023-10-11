d767fa20c9749eb4a2d29f7518f5a7bd4df56235
Denis Vasilenko <denisvasilenko9@gmail.com>
2023-10-11 21:00:19 +0300

    Added upload file method of Cloud.Yandex.Store v1.1

--- src/main/java/com/denisvasilenko/BlogApp/controler/blogController.java
+++ src/main/java/com/denisvasilenko/BlogApp/controler/blogController.java
@@ -1,6 +1,13 @@
 package com.denisvasilenko.BlogApp.controler;
 
-import com.denisvasilenko.BlogApp.yandexCloudStore.cloud;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.AmazonS3EncryptionClientV2Builder;
+import com.denisvasilenko.BlogApp.yandexCloudStore.CloudYandexStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,8 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class blogController {
     @GetMapping("/test")
     public String TestContoller(){
-        cloud clouud = new cloud();
-        clouud.cloudCreateBacket();
+        CloudYandexStore cloudYandexStore=new CloudYandexStore("blogapp");
+        cloudYandexStore.uploadFiles("How to Create bucket","Hello, today i show you how to create bucket in Yandex.Store Object!" +
+                "it is a beautiful service");
         return "Method end";
     }
 }
