f1a1c655b447ec7d64c4c159e480a5f252afba29
Denis Vasilenko <denisvasilenko9@gmail.com>
2023-12-14 09:11:30 +0300

    Writing tests for ArticleService 1.3 test

--- src/test/java/com/denisvasilenko/BlogApp/Services/ArticleServiceTest.java
+++ src/test/java/com/denisvasilenko/BlogApp/Services/ArticleServiceTest.java
@@ -1,8 +1,11 @@
 package com.denisvasilenko.BlogApp.Services;
 
+import com.denisvasilenko.BlogApp.models.User;
 import com.denisvasilenko.BlogApp.services.ArticleService;
+import com.denisvasilenko.BlogApp.yandexCloudStore.DTO.CloudUploadRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -14,5 +17,15 @@ public class ArticleServiceTest {
     @Autowired
     ArticleService articleService;
 
-    //TODO:TEST
+    @Mock
+    User user;
+
+//    CloudUploadRequest cloudUploadRequest = new CloudUploadRequest(
+//            "blogapp",
+//            "articleIdentifier",
+//            "createArticleDto.articleName",
+//            "createArticleDto.articleContent()",
+//            userOwner,
+//            getCurrentDate()
+//    );
 }
